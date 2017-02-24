e763ae187538b947733af81e8bdf6b5f8a88ab7b
ManishaRana1195 <manisha1195@gmail.com>
2017-02-24 11:14:09 +0530

    changes in providerTest

--- app/src/androidTest/java/com/geekskool/dhobi/Providers/DataProviderTest.java
+++ app/src/androidTest/java/com/geekskool/dhobi/Providers/DataProviderTest.java
@@ -27,44 +27,51 @@ import static com.geekskool.dhobi.Contracts.Contract.CourseEntry.COLUMN_START_DA
 @RunWith(AndroidJUnit4.class)
 public class DataProviderTest extends ProviderTestCase2<DataProvider> {
 
-    private ContentValues values;
-
     public DataProviderTest() {
         super(DataProvider.class, Contract.CONTENT_AUTHORITY);
     }
 
     @Before
-    @Override
     public void setUp() throws Exception {
         setContext(InstrumentationRegistry.getTargetContext());
         super.setUp();
-        values = new ContentValues();
-        String cName = "SampleName";
-        int days = 10;
-        String loc = "bangalore";
-        Calendar cal = Calendar.getInstance();
-
-        values.put(COLUMN_NAME, cName);
-        values.put(COLUMN_START_DATE, cal.getTimeInMillis());
-        values.put(COLUMN_END_DATE, cal.getTimeInMillis() + 1000);
-        values.put(COLUMN_DURATION, days);
-        values.put(COLUMN_LOCATION, loc);
     }
 
     @Test
-    public void testShouldBeAbleToInsertCourse() {
-        Uri uri = getMockContentResolver().insert(Contract.CourseEntry.CONTENT_URI, values);
+    public void test_should_be_able_to_insert_course() {
+        Uri uri = getMockContentResolver().insert(Contract.CourseEntry.CONTENT_URI, getValidValues());
+
         long id = ContentUris.parseId(uri);
 
         assertNotNull(uri);
         assertTrue(id > 0);
     }
 
+
     @Test(expected = UnsupportedOperationException.class)
-    public void shouldNotBeAbleToInsertCourse() {
+    public void should_throw_unsupported_operation_exception_if_course_fields_are_null() {
+        getMockContentResolver().insert(Contract.CourseEntry.CONTENT_URI, getNullValues());
+    }
+
+    private ContentValues getNullValues() {
         ContentValues cv = new ContentValues();
+        cv.putNull(COLUMN_NAME);
+        cv.putNull(COLUMN_START_DATE);
+        cv.putNull(COLUMN_END_DATE);
+        cv.putNull(COLUMN_DURATION);
+        cv.putNull(COLUMN_LOCATION);
+        return cv;
+    }
+
+    private ContentValues getValidValues() {
+        ContentValues cv = new ContentValues();
+        Calendar cal = Calendar.getInstance();
         cv.put(COLUMN_NAME, "SampleName");
-        getMockContentResolver().insert(Contract.CourseEntry.CONTENT_URI, cv);
+        cv.put(COLUMN_START_DATE, cal.getTimeInMillis());
+        cv.put(COLUMN_END_DATE, cal.getTimeInMillis() + 1000);
+        cv.put(COLUMN_DURATION, 10);
+        cv.put(COLUMN_LOCATION, "bangalore");
+        return cv;
     }
 
 }
